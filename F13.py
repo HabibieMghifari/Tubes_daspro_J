842e1d59a50922f87eda8bdf9cec6e8938395e1d
KodokElit <166517678+KodokElit@users.noreply.github.com>
2024-05-09 00:24:57 +0700

    Update F13.py

--- F13.py
+++ F13.py
@@ -24,10 +24,10 @@ def load_data(data, x):
                     nilai += char
     return list
 
-def csvwriter(filename, username, password):
+def monster_adder(filename, type, atk_power, def_power, hp):
     # Tentukan role dan oc
-    role = 'agent'
-    oc = 0
+    #role = 'agent'
+    #oc = 0
 
     # Periksa apakah file csv ada dan tentukan ID berikutnya
     if os.path.exists(filename):
@@ -41,13 +41,31 @@ def csvwriter(filename, username, password):
     else:
         next_id = 2
     with open(filename, 'a') as file:
-        file.write(f"{next_id};{username};{password};{role};{oc}\n")
+        file.write(f"{next_id};{type};{atk_power};{def_power};{hp}\n")
 
-
-from F02 import status
 from F02 import admin
+z=0
 if admin == True :
-    monster_name=load_data('monster.csv',1)
-    print(monster_name)
-#load_data('monster.csv',)
-
+    print("Selamat Datang Pada Monster management!\n1. Tampilkan Semua Monster\n2. Tambah Monster\n3. Keluar")
+    while z<4:
+        z=int(input(f"Masukkan Pilihan Anda!\n"))
+        if z == 1 :
+            monster_name=load_data('monster.csv',1)
+            monster_atk=load_data('monster.csv',2)
+            monster_def=load_data('monster.csv',3)
+            monster_hp=load_data('monster.csv',4)
+            print(f"{monster_name}\n{monster_atk}\n{monster_def}\n{monster_hp}")
+        elif z==2:
+            l=input("Nama Monster : ")
+            m=int(input("atk_power : "))
+            n= int(input("def_power : "))
+            o= int(input("hp : "))
+            p=input(f"apakah yakin dengan penambahan monster ini? ketik Y/N\n")
+            if p=="Y" :
+                monster_adder('monster.csv',l,m,n,o)
+                print("Monster berhasil ditambahkan!")
+            elif p=="N" :
+                print("Monster gagal ditambahkan!")
+        elif z==3:
+            print("Selamat tinggal!")
+            break
